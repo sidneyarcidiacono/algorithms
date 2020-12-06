c0d22d0c0af443a9f85f9ce38255f150e05b5cef
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-12-06 08:44:19 -0800

    Finished cartesian product algorithms & notes.

--- array_algorithms.md
+++ array_algorithms.md
@@ -6,3 +6,31 @@
 ### What are Set (Array) Algorithms About?
 
 A set is a **collection of values** (objects, numbers) which forms an **entity itself**
+
+You can commonly use arrays and sets interchangeably because most arrays are sets. You rarely have arrays of unconnected or random data.
+
+### The Cartesian Product Algorithm:
+
+For two sets, we want to get all of the possible combinations of those two sets. For example, if we have a clothing store,
+
+setA is colors for a product: ['red', 'blue']
+
+and setB are the sizes: ['s', 'm', 'l'],
+
+We want to be able to derive [['s', 'red'], ['s', 'blue']...] and so on, so that we obtain a list which contains nested lists of all of the combinations of our two sets.
+
+### The Cartesian Product Algorithm Time Complexity:
+
+Big O for this currently is going to be O(n * m) because although we have nested loops, we're running each loop on a separate input or a separate set.
+
+We also have the limitation that currently we can only take two sets as input. They don't have to be the same size, but we can only ever derive the Cartesian Product of two sets.
+
+We could actually argue for quadratic time and space complexity here if we want to consider the worst case and assume that the length of the longest array is the length of both arrays without differentiating.
+
+### Cartesian Product (Without Limits) Time Complexity:
+
+We're going to assume that the length of the longest array we're given is the length of all of the arrays (to generalize the worst case scenario).
+
+Our worst case generalization also includes the amount of arrays we have : so our time complexity if we don't know the number of arrays is going to be O(n^x) where x is the number of sets we're getting.
+
+It's the same for the space complexity: O(n^x).
--- practice-setup/app.js
+++ practice-setup/app.js
@@ -1,39 +1,62 @@
-const sort = arr => {
-  // Set our two base conditions.
-  if (arr.length < 2) {
-    return arr
-  }
-  if (arr.length === 2) {
-    return arr[0] > arr[1] ? [arr[1], arr[0]] : arr
-  }
-  const middle = Math.floor(arr.length / 2)
-  const leftArray = arr.slice(0, middle)
-  const rightArray = arr.slice(middle)
+// CARTESIAN PRODUCT ORIGINAL
 
-  const leftSortedArray = sort(leftArray)
-  const rightSortedArray = sort(rightArray)
+// const cartProduct = (setA, setB) => {
+   // We're going to assume we always get a valid set with inputs
+  // so we're going to omit an if check to see if we have valid arrays.
+//
+//   const product = []
+//
+//   for (const setAEl of setA) {
+//     for (const setBEl of setB) {
+//       product.push([setAEl, setBEl])
+//     }
+//   }
+//
+//   return product
+// }
+//
+//
+// const colors = ['blue', 'red']
+// const sizes = ['s', 'm', 'l']
+// console.log(cartProduct(colors, sizes)) // [[blue, m], ...]
 
-  const mergedArr = []
-  let leftArrIndex = 0
-  let rightArrIndex = 0
-  while (leftArrIndex < leftSortedArray.length || rightArrIndex < rightSortedArray.length) {
-    if (leftArrIndex >= leftSortedArray.length|| leftSortedArray[leftArrIndex] > rightSortedArray[rightArrIndex]) {
-      mergedArr.push(rightSortedArray[rightArrIndex])
-      rightArrIndex++
-    } else {
-      mergedArr.push(leftSortedArray[leftArrIndex])
-      leftArrIndex++
+// Big O for this currently is going to be O(n * m) because although we have nested loops, we're running each loop on a separate input or a separate set.
+// Space Complexity: going to be O(n*m) because we create one new value which is stored in the product array per each combination of our two sets.
+
+// We could argue for both space and time complexity to be quadratic if we want to consider the worst case
+// and assume that the length of the longest array is the length of both arrays without differentiating.
+
+// CARTESIAN PRODUCT WITHOUT LIMITS
+
+const cartProduct = (setA, setB) => {
+  // We're going to assume we always get a valid set with inputs
+  // so we're going to omit an if check to see if we have valid arrays.
+
+  const product = []
+
+  for (let setAEl of setA) {
+    if (!Array.isArray(setAEl)) {
+      setAEl = [setAEl]
+    }
+    for (const setBEl of setB) {
+      product.push([...setAEl, setBEl])
     }
   }
 
-  return mergedArr
+  return product
 }
 
-// Recursive step time complexity: O(n ^ logb(a)) => O(n^log2(2)) => O(n^1) => O(n)
-// Outside time complexity: O(n)
-// Overall time complexity can be derived using the Master theorem, where we have the
-// same work inside and outside of the recursion, so we have O(n * log n)
+const cartesian = (...sets) => {
+  let tempProduct = sets[0]
 
-const sortedArray = sort([-10, 33, 5, 10, 3, -19, 99])
+  for (let i = 1; i < sets.length; i++) {
+    tempProduct = cartProduct(tempProduct, sets[i])
+  }
 
-console.log(sortedArray)
+  return tempProduct
+}
+
+const colors = ['blue', 'red']
+const sizes = ['s', 'm', 'l', 'xl']
+const styles = ['round neck', 'v neck']
+console.log(cartesian(colors, sizes, styles)) // [[blue, m], ...]
